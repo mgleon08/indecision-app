f40a5d8a37e3e58c23f5c577656b4d472c5f72fa
Leon Ji <mgleon08@gmail.com>
2019-08-31 22:48:14 +0800

    Exploring JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,34 @@ console.log('running');
 // JSX - javascript XML
 
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  ' this is JSX'
+  React.createElement(
+    'h1',
+    null,
+    ' this is JSX jj'
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' info '
+  )
+);
+
+var template2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Name: Leon Ji'
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' Location: Taiwan '
+  )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 console.log('running');
 // JSX - javascript XML
 
-var template = <p> this is JSX</p>;
+var template = (
+  <div>
+    <h1> this is JSX jj</h1>
+    <p> info </p>
+  </div>
+);
+
+var template2 = (
+  <div>
+    <h1>Name: Leon Ji</h1>
+    <p> Location: Taiwan </p>
+  </div>
+);
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
