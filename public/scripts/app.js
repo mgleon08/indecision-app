8796fd95d663a534d0203f6000623420b8cf9057
Leon Ji <mgleon08@gmail.com>
2019-09-01 09:22:07 +0800

    Manual Data Binding

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -53,6 +53,54 @@ var template2 = React.createElement(
   )
 );
 
+var count = 0;
+var addOne = function addOne() {
+  count++;
+  console.log('addOne', count);
+  renderCounterApp();
+};
+
+var minusOne = function minusOne() {
+  console.log('minusOne', count);
+  count--;
+  renderCounterApp();
+};
+
+var reset = function reset() {
+  console.log(count);
+  count = 0;
+  renderCounterApp();
+};
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template2, appRoot);
+var renderCounterApp = function renderCounterApp() {
+  var template3 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      ' +1 '
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      ' -1 '
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      ' reset '
+    )
+  );
+  ReactDOM.render(template3, appRoot);
+};
+
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -27,6 +27,37 @@ let template2 = (
   </div>
 );
 
-let appRoot = document.getElementById('app');
+let count = 0;
+let addOne = () => {
+  count++;
+  console.log('addOne', count);
+  renderCounterApp();
+};
 
-ReactDOM.render(template2, appRoot);
+let minusOne = () => {
+  console.log('minusOne', count);
+  count--;
+  renderCounterApp();
+};
+
+let reset = () => {
+  console.log(count);
+  count = 0;
+  renderCounterApp();
+};
+
+const appRoot = document.getElementById('app');
+
+const renderCounterApp = () => {
+  const template3 = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}> +1 </button>
+      <button onClick={minusOne}> -1 </button>
+      <button onClick={reset}> reset </button>
+    </div>
+  );
+  ReactDOM.render(template3, appRoot);
+};
+
+renderCounterApp();
