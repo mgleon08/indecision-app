f2c5b5901527e75c4f3745a48eb0e1eb25442159
Leon Ji <mgleon08@gmail.com>
2019-09-10 00:19:45 +0800

    Refactoring Other Stateless Functional Components

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const Action = props => {
-  return (
-    <div>
-      <button onClick={props.handlePick} disabled={!props.hasOptions}>
-        what should i do ?
-      </button>
-    </div>
-  );
-};
+const Action = props => (
+  <div>
+    <button onClick={props.handlePick} disabled={!props.hasOptions}>
+      what should i do ?
+    </button>
+  </div>
+);
 
 export default Action;
